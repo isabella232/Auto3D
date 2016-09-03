8664fed84dda58b4dc7fbb852184615c0ed7358e
Stéphane Lenclud <github@lenclud.com>
2016-09-03 18:24:53 +0200

    SharpLibDisplay now allows adding commands. That allows for easily disabling some 3D modes and re-enabling them later if needed.

--- Auto3D-SharpLibDisplay/SharpLibDisplayRemoteKeypad.Designer.cs
+++ Auto3D-SharpLibDisplay/SharpLibDisplayRemoteKeypad.Designer.cs
@@ -29,28 +29,117 @@ namespace MediaPortal.ProcessPlugins.Auto3D.Devices
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SharpLibDisplayKeypad));
-            this.labelExplainations = new System.Windows.Forms.Label();
-            this.SuspendLayout();
-            // 
-            // labelExplainations
-            // 
-            this.labelExplainations.AutoSize = true;
-            this.labelExplainations.Location = new System.Drawing.Point(3, 71);
-            this.labelExplainations.Name = "labelExplainations";
-            this.labelExplainations.Size = new System.Drawing.Size(245, 91);
-            this.labelExplainations.TabIndex = 38;
-            this.labelExplainations.Text = resources.GetString("labelExplainations.Text");
-            // 
-            // SharpLibDisplayKeypad
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.labelExplainations);
-            this.Name = "SharpLibDisplayKeypad";
-            this.Size = new System.Drawing.Size(256, 256);
-            this.ResumeLayout(false);
-            this.PerformLayout();
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SharpLibDisplayKeypad));
+      this.labelExplainations = new System.Windows.Forms.Label();
+      this.button2Dto3DSBS = new System.Windows.Forms.Button();
+      this.button3DSBSto2D = new System.Windows.Forms.Button();
+      this.button2Dto3DTAB = new System.Windows.Forms.Button();
+      this.button3DTABto2D = new System.Windows.Forms.Button();
+      this.button2Dto3D = new System.Windows.Forms.Button();
+      this.button3Dto2D = new System.Windows.Forms.Button();
+      this.label1 = new System.Windows.Forms.Label();
+      this.SuspendLayout();
+      // 
+      // labelExplainations
+      // 
+      this.labelExplainations.AutoSize = true;
+      this.labelExplainations.Location = new System.Drawing.Point(3, 90);
+      this.labelExplainations.Name = "labelExplainations";
+      this.labelExplainations.Size = new System.Drawing.Size(242, 91);
+      this.labelExplainations.TabIndex = 38;
+      this.labelExplainations.Text = resources.GetString("labelExplainations.Text");
+      // 
+      // button2Dto3DSBS
+      // 
+      this.button2Dto3DSBS.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.button2Dto3DSBS.Location = new System.Drawing.Point(6, 3);
+      this.button2Dto3DSBS.Name = "button2Dto3DSBS";
+      this.button2Dto3DSBS.Size = new System.Drawing.Size(72, 24);
+      this.button2Dto3DSBS.TabIndex = 39;
+      this.button2Dto3DSBS.Tag = "2D>3DSBS";
+      this.button2Dto3DSBS.Text = "2D>3DSBS";
+      this.button2Dto3DSBS.UseVisualStyleBackColor = true;
+      // 
+      // button3DSBSto2D
+      // 
+      this.button3DSBSto2D.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.button3DSBSto2D.Location = new System.Drawing.Point(84, 3);
+      this.button3DSBSto2D.Name = "button3DSBSto2D";
+      this.button3DSBSto2D.Size = new System.Drawing.Size(72, 24);
+      this.button3DSBSto2D.TabIndex = 40;
+      this.button3DSBSto2D.Tag = "3DSBS>2D";
+      this.button3DSBSto2D.Text = "3DSBS>2D";
+      this.button3DSBSto2D.UseVisualStyleBackColor = true;
+      // 
+      // button2Dto3DTAB
+      // 
+      this.button2Dto3DTAB.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.button2Dto3DTAB.Location = new System.Drawing.Point(6, 33);
+      this.button2Dto3DTAB.Name = "button2Dto3DTAB";
+      this.button2Dto3DTAB.Size = new System.Drawing.Size(72, 24);
+      this.button2Dto3DTAB.TabIndex = 41;
+      this.button2Dto3DTAB.Tag = "2D>3DTAB";
+      this.button2Dto3DTAB.Text = "2D>3DTAB";
+      this.button2Dto3DTAB.UseVisualStyleBackColor = true;
+      // 
+      // button3DTABto2D
+      // 
+      this.button3DTABto2D.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.button3DTABto2D.Location = new System.Drawing.Point(84, 33);
+      this.button3DTABto2D.Name = "button3DTABto2D";
+      this.button3DTABto2D.Size = new System.Drawing.Size(72, 24);
+      this.button3DTABto2D.TabIndex = 42;
+      this.button3DTABto2D.Tag = "3DTAB>2D";
+      this.button3DTABto2D.Text = "3DTAB>2D";
+      this.button3DTABto2D.UseVisualStyleBackColor = true;
+      // 
+      // button2Dto3D
+      // 
+      this.button2Dto3D.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.button2Dto3D.Location = new System.Drawing.Point(6, 63);
+      this.button2Dto3D.Name = "button2Dto3D";
+      this.button2Dto3D.Size = new System.Drawing.Size(72, 24);
+      this.button2Dto3D.TabIndex = 43;
+      this.button2Dto3D.Tag = "2D>3D";
+      this.button2Dto3D.Text = "2D>3D";
+      this.button2Dto3D.UseVisualStyleBackColor = true;
+      // 
+      // button3Dto2D
+      // 
+      this.button3Dto2D.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+      this.button3Dto2D.Location = new System.Drawing.Point(84, 63);
+      this.button3Dto2D.Name = "button3Dto2D";
+      this.button3Dto2D.Size = new System.Drawing.Size(72, 24);
+      this.button3Dto2D.TabIndex = 44;
+      this.button3Dto2D.Tag = "3D>2D";
+      this.button3Dto2D.Text = "3D>2D";
+      this.button3Dto2D.UseVisualStyleBackColor = true;
+      // 
+      // label1
+      // 
+      this.label1.AutoSize = true;
+      this.label1.Location = new System.Drawing.Point(3, 181);
+      this.label1.Name = "label1";
+      this.label1.Size = new System.Drawing.Size(237, 65);
+      this.label1.TabIndex = 45;
+      this.label1.Text = resources.GetString("label1.Text");
+      // 
+      // SharpLibDisplayKeypad
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.Controls.Add(this.label1);
+      this.Controls.Add(this.button3Dto2D);
+      this.Controls.Add(this.button2Dto3D);
+      this.Controls.Add(this.button3DTABto2D);
+      this.Controls.Add(this.button2Dto3DTAB);
+      this.Controls.Add(this.button3DSBSto2D);
+      this.Controls.Add(this.button2Dto3DSBS);
+      this.Controls.Add(this.labelExplainations);
+      this.Name = "SharpLibDisplayKeypad";
+      this.Size = new System.Drawing.Size(256, 256);
+      this.ResumeLayout(false);
+      this.PerformLayout();
 
         }
 
@@ -58,5 +147,12 @@ namespace MediaPortal.ProcessPlugins.Auto3D.Devices
         #endregion
 
         private System.Windows.Forms.Label labelExplainations;
-    }
+    private System.Windows.Forms.Button button2Dto3DSBS;
+    private System.Windows.Forms.Button button3DSBSto2D;
+    private System.Windows.Forms.Button button2Dto3DTAB;
+    private System.Windows.Forms.Button button3DTABto2D;
+    private System.Windows.Forms.Button button2Dto3D;
+    private System.Windows.Forms.Button button3Dto2D;
+    private System.Windows.Forms.Label label1;
+  }
 }
